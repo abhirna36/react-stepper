3b7c9bdac41c8bf1ad42c3989cdf746d381be535
abhirna36 <abhishek17rna@gmail.com>
2021-05-07 19:57:58 +0530

    progress bar added

--- src/component/Stepper.js
+++ src/component/Stepper.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Stepper() {
+function Stepper(props) {
+    const {numberOfSteps,completedStepColor,inactiveStepColor,completedProgressColor,remainingProgressColor,activeStep,labelText} = props;
+
+    const [progressSteps, setProgressSteps] = useState([]);
+
+    useEffect(() => {
+        let arr = [];
+        for(let i = 0; i<numberOfSteps; i++ ) {
+            arr.push(i);
+        }
+        setProgressSteps(arr);
+    },[]);
+
+    console.log("progress step length",progressSteps);
+
     return (
-        <div>
-            hello team
+        <div style={{flex: 1}}>
+        <div style={{display: "flex", flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 12, flex:1}}>
+            {/* {progressSteps.map((step, index) => {
+                return (
+                    <div style={{display: "flex", flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 12}}>
+                        <div style={{height: 30, width:30, borderRadius: 15,backgroundColor: completedStepColor, alignSelf: 'center'}} />
+                        {index !== progressSteps.length-1 && (
+                            <div style={{display: 'flex', flex: 1, width: 100,flexDirection: 'row', backgroundColor: activeStep > 1 ? completedProgressColor: '#cccccc'}}>
+                            <div style={{height: 5, width: 10, backgroundColor: completedProgressColor}}/>
+                        </div>
+                        )}
+                    </div>
+                )
+            })} */}
+            {/* <div style={{display: 'flex', flex: 1, width: 100,flexDirection: 'row', backgroundColor: activeStep > 1 ? completedProgressColor: '#cccccc'}}>
+                            <div style={{height: 5, width: 10, backgroundColor: completedProgressColor}}/>
+                        </div> */}
+            <div style={{height: 30, width:30, borderRadius: 15,backgroundColor: completedStepColor, alignSelf: 'center'}}></div>
+                <div style={{display: 'flex', flex: 1, flexDirection: 'row', backgroundColor: activeStep > 1 ? completedProgressColor: remainingProgressColor}}>
+                    <div style={{height: 5, width: 10, backgroundColor: completedProgressColor}}/>
+                </div>
+            <div style={{height: 30,width:30, borderRadius: 15,backgroundColor: activeStep > 1 ? completedStepColor: inactiveStepColor}} />
+            <div style={{display: 'flex', flex: 1, flexDirection: 'row', backgroundColor: activeStep > 2? completedProgressColor: remainingProgressColor}}>
+                <div style={{height: 5, width: 35, backgroundColor: activeStep > 1 ? completedProgressColor: remainingProgressColor}} />
+            </div>
+            <div style={{height: 30, width: 30, borderRadius: 15,backgroundColor: activeStep > 2 ? completedStepColor: inactiveStepColor}} />
         </div>
+        
+        <div style={{display: 'flex',flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 8}}>
+            {progressSteps.map((step, index) => {
+                return <p style={{color: activeStep === index ? 'black' : 'inactiveStepColor'}}>{labelText} {step+1}</p>
+            })}
+        </div>
+    </div> 
     )
 }
 
--- src/views/Example.js
+++ src/views/Example.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import Stepper from '../component/Stepper'
+import React from "react";
+import Stepper from "../component/Stepper";
 
 function Example() {
-    return (
-        <div>
-           <Stepper numberOfSteps={4} />
-        </div>
-    )
+  return (
+    <div style={{display: 'flex', margin: 12,}}>
+      <Stepper
+        numberOfSteps={4}
+        completedStepColor={"#007bff"}
+        inactiveStepColor={"#cccccc"}
+        completedProgressColor={"#007bff"}
+        remainingProgressColor={"#cccccc"}
+        activeStep={2}
+        labelText={'Step'}
+      />
+    </div>
+  );
 }
 
-export default Example
+export default Example;
